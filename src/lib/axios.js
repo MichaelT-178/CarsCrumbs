4ea0401d4abbb3c9bdedaff4a89e031ef8bf789f
Michael Totaro <mstotaro@gmail.com>
2024-12-29 01:02:19 -0500

    Update axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://xwes07wf33.execute-api.us-east-1.amazonaws.com/dev',
+  baseURL: 'https://azr5xytq99.execute-api.us-east-1.amazonaws.com/dev',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
