1ba9476dbc328460d5bdbaee4c4e3897bb46ed2d
Michael Totaro <mstotaro@gmail.com>
2026-03-29 18:29:01 -0400

    Updated vite.config.js server

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	base: '/',
 	plugins: [vue()],
+	resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname
+    }
+  },
+  server: {
+    host: "localhost", 
+  }
 })
